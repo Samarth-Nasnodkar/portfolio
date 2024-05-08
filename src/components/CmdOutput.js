bf925d46cf73c92fbbbb4a74af7a8942c1451221
Samarth Nasnodkar <samarthnasnodkar@gmail.com>
2024-05-08 15:42:40 +0530

    working on contact component

--- src/components/CmdOutput.js
+++ src/components/CmdOutput.js
@@ -3,12 +3,14 @@ import '../css/CmdOutput.css';
 import Ls from './commands/Ls';
 import Welcome from './commands/Welcome';
 import About from './commands/About';
+import Contact from './commands/Contact';
 
 export default function CmdOutput(props) {
   let cmdComponent = (<div>{props.cmd}</div>);
   if (props.cmd.toLowerCase() === 'ls') cmdComponent = <Ls />
   if (props.cmd.toLowerCase() === 'welcome') cmdComponent = <Welcome />
   if (props.cmd.toLowerCase() === 'about') cmdComponent = <About />
+  if (props.cmd.toLowerCase() === 'contact') cmdComponent = <Contact />
   return (
     <div className='CmdOutput'>
       {cmdComponent}
